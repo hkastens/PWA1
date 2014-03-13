42e6dadfec7324b9ff994809466c32b01535e72b
hkastens <hkastens@fullsail.edu>
2014-03-12 20:44:26 -0500

    Changed code so it supports odjects.

--- WK2/goal4_assign_duel3 /js/main.js
+++ WK2/goal4_assign_duel3 /js/main.js
@@ -11,41 +11,41 @@ fighter2={name: 'Batman',damage:20,health:100};
 
 console.log("FIGHT!!!");
 
-    var fighter1 = ["Spiderman", 20, 100];
-    var fighter2 = ["Batman", 20, 100];
+    //var fighter1 = ["Spiderman", 20, 100];
+    //var fighter2 = ["Batman", 20, 100];
 
     //Initiate Round
     var round=0;
 
     function fight(){
     console.log('in the fight function');
-    alert(fighter1 [0]+":"+fighter1 [2]+" *START* "+fighter2 [0]+":"+fighter2 [2]);
+    //alert(fighter1 [0]+":"+fighter1 [2]+" *START* "+fighter2 [0]+":"+fighter2 [2]);
         for (var i=0; i < 10; i++){
 
             //random formula is - Math.floor(Math.random() * (max - min) + min);
 
-            var minDamage1 = fighter1[1] * .5;
-            var minDamage2 = fighter2[1] * .5;
-            var f1 = Math.floor(Math.random()*(fighter1[1]-minDamage1)+minDamage1);
-            var f2 = Math.floor(Math.random()*(fighter2[1]-minDamage2)+minDamage2);
+            var minDamage1 = fighter1.damage * .5;
+            var minDamage2 = fighter2.damage * .5;
+            var f1 = Math.floor(Math.random()*(fighter1.damage-minDamage1)+minDamage1);
+            var f2 = Math.floor(Math.random()*(fighter2.damage-minDamage2)+minDamage2);
 
 
 
             //inflict damage
-            fighter1[2]-=f1;
-            fighter2[2]-=f2;
+            fighter1.health-=f1;
+            fighter2.health-=f2;
 
-            console.log(fighter1[0]+":"+fighter1[2]+" "+fighter2[0]+":"+fighter2[2]);
+            console.log(fighter1.name+":"+fighter1.health+" "+fighter2.name+":"+fighter2.health);
 
     var results = winnerCheck();
             console.log(results);
 
             if (results ==="no winner"){
               round++;
-                alert(fighter1[0]+":"+fighter1[2]+" *ROUND "+round+" OVER* "+ fighter2[0]+":"+fighter2[2]);
+               // alert(fighter1[0]+":"+fighter1[2]+" *ROUND "+round+" OVER* "+ fighter2[0]+":"+fighter2[2]);
 
             }else{
-                alert(results);
+               // alert(results);
                 break;
             };
         };
